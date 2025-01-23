a0cc07613afe0e584fb7a25897912a51f3f1dbd4
Aakash <aaka2005@gmail.com>
2025-01-23 12:51:33 -0500

    Finds the exit and entry points for basic mazes

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -19,23 +19,26 @@ public class Maze {
             int rows = 0;
             int cols = 0;
 
-            
             while ((line = reader.readLine()) != null) {
                 mazeBuilder.append(line).append("\n");
                 rows++;
                 cols = line.length();
             }
 
-            
             maze = new char[rows][cols];
             String[] lines = mazeBuilder.toString().split("\n");
 
-           
             for (int i = 0; i < rows; i++) {
                 maze[i] = lines[i].toCharArray();
             }
 
+            
+            findEntryAndExit();
+
             System.out.println("Maze read successfully");
+            System.out.println("Entry point found at: " + entry);
+            System.out.println("Exit point found at: " + exit);
+
         } catch (Exception e) {
             System.out.println("Error reading maze: " + e.getMessage());
         }
@@ -65,5 +68,33 @@ public class Maze {
 
     public Point getExit() {
         return exit;
+    }
+
+    private void findEntryAndExit() {
+        int rows = maze.length; //gets the number of rows
+        int cols = maze[0].length; //just gets the first row length
+        
+        //check left border for an entry point
+        for (int i = 0; i < rows; i++) {
+            //in the first column, check if there is a space
+            if (maze[i][0] == ' ') {
+                entry = new Point(0, i);
+                break;
+            }
+        }
+
+        //check right border for an exit point
+        for (int i = 0; i < rows; i++) {
+            //in the last column, check if there is a space
+            if (maze[i][cols-1] == ' ') {
+                exit = new Point(cols-1, i);
+                break;
+            }
+        }
+
+        //if there is no entry or exit point, throw an error
+        if (entry == null || exit == null) {
+            throw new IllegalStateException("Maze must have both an entry point on the left and an exit point on the right");
+        }
     }
 }
